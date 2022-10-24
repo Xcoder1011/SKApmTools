112468e7a26b2ba31a5c99f1f2236619ccce5d2b
KUN <kun@KUNdeMacBook-Pro-2.local>
2022-10-24 13:42:19 +0800

    Initial commit

---
+++ Example/SKApmTools/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SKApmTools
+//
+//  Created by Xcoder1011 on 10/24/2022.
+//  Copyright (c) 2022 Xcoder1011. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
